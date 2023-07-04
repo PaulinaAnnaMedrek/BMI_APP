f709eea14fcb304e20926576adb606554bc2eba5
PaulinaAnnaMedrek <138514090+PaulinaAnnaMedrek@users.noreply.github.com>
2023-07-04 18:52:35 +0200

    Add files via upload

---
+++ MVVM/Models/BMI.cs
@@ -0,0 +1,72 @@
+﻿using PropertyChanged;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp6.MVVM.Models
+{
+    [AddINotifyPropertyChangedInterface]
+    public class BMI
+    {
+        private float result;
+        private string rText;
+
+        public float Growth { get; set; }
+        public float Weight { get; set; }
+
+        public float Result
+        {
+            get
+            {
+                return ((Weight / Growth) / Growth) * 10000;
+            }
+        }
+
+
+        public string RText
+        {
+            get
+            {
+                
+                if (Result <= 16)
+                {
+                    return ( "Severe Undereight");
+                }
+                else if (Result > 16 && Result <= 17)
+                {
+                    return ( "Moderate Underveight");
+                }
+                else if (Result > 17 && Result <= 18.5)
+                {
+                    return ("Mild Undereight");
+                }
+                else if (Result > 18.5 && Result <= 25)
+                {
+                    return ("Normal");
+                }
+                else if (Result > 25 && Result <= 30)
+                {
+                    return ( "Overweight");
+                }
+                else if (Result > 30 && Result <= 35)
+                {
+                    return ( "Obese Class I");
+                }
+                else if (Result > 35 && Result <= 40)
+                {
+                    return ( "Obese Class II");
+                }
+                else
+                {
+                    return ( "Obese Class III");
+                }
+
+            }
+        }
+
+
+    }
+}
---
+++ MVVM/Models/Categoriess.cs
@@ -0,0 +1,21 @@
+﻿using PropertyChanged;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp6.MVVM.Models
+{
+    [AddINotifyPropertyChangedInterface]
+    public class Categoriess
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public string Colors { get; set; }
+        public int WaitingTask { get; set; }
+        public float PercProgress { get; set; }
+
+        public bool Select { get; set; }
+    }
+}
